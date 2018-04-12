8acd68342e1e4d54f7dc78ef635d3f305c8bd6e9
Unknown <nethra2012@aol.com>
2018-04-12 16:37:10 -0400

    Uploading to the C L O U D

---
+++ Unity 1/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public GameObject grenadePreFab;
+    public float speed = 1f;
+    private Vector3 playerposition;
+
+	// Use this for initialization
+	void Start () {
+        playerposition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xpos = transform.position.x + (Input.GetAxis("Horizontal") * speed);
+        playerposition = new Vector3(Mathf.Clamp(xpos, -7.5f, 7.5f), 3.1f, 0f);
+        transform.position = playerposition;
+
+        if(Input.GetKeyDown("space"))
+        {
+            grenadePreFab.GetComponent<Transform>().position = new Vector3(playerposition.x, 2.72f, 0);
+            Instantiate(grenadePreFab);
+        }
+	}
+}
